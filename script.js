a2d005fe05266bea30d066a1e39cb02771fc63c1
zaynab kaissar <zaynabkaissar@gmail.com>
2025-04-20 22:57:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const products = [
+    {
+      name: "Boho Beaded Bracelet",
+      image: "images/beadsflower.jpg",
+      price: "8.00 MAD"
+    },
+    {
+      name: "cherry braclets",
+      image: "images/braclets.jpg",
+      price: "5.00 MAD"
+    },
+  ];
+  
+  const productList = document.getElementById('productList');
+  products.forEach(product => {
+    const card = document.createElement('div');
+    card.className = 'product';
+    card.innerHTML = `
+      <img src="${product.image}" alt="${product.name}" />
+      <h4>${product.name}</h4>
+      <p>${product.price}</p>
+    `;
+    productList.appendChild(card);
+  });
+  
+  function scrollToShop() {
+    document.getElementById("shop").scrollIntoView({ behavior: 'smooth' });
+  }
+  
+  document.getElementById("year").textContent = new Date().getFullYear();
+  
